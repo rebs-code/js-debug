46a2114054a7f2de98972f23c0186d5b88ac42b0
rebs-code <jack.rebussi@gmail.com>
2023-11-23 19:01:56 +0100

    feat: debug 03

---
+++ bundle-03/main.js
@@ -0,0 +1,89 @@
+/*******************************************************************************
+​
+    Per questo esercizio, rispondi a queste domande:
+    1- Che cosa fa questo codice?
+    2- Sono presenti errori di sintassi?
+    3- Sono presenti errori logici?
+​
+    Inserisci i tuoi commenti con le risposte alla fine dell'esercizio
+​
+*******************************************************************************/
+
+
+// ESERCIZIO 1 (suggerimento: ci sono 6 errori)
+const cars = [
+    {
+        manufacturer: 'Ford',
+        model: 'Fiesta',
+        type: 'diesel'
+    },
+    {
+        manufacturer: 'Audi',
+        model: 'A1',
+        type: 'benzina'
+    },
+    {
+        manufacturer: 'Volkswagen',
+        model: 'Golf',
+        type: 'Benzina'
+    },
+    {
+        manufacturer: 'Fiat',
+        model: 'Panda',
+        type: 'metano'
+    },
+    {
+        manufacturer: 'Fiat',
+        model: 'Multipla',
+        type: 'GPL'
+    },
+    {
+        manufacturer: 'Tesla',
+        model: 'Model 3',
+        type: 'elettrico'
+    },
+    {
+        manufacturer: 'Volkswagen',
+        model: 'Polo',
+        type: 'benzina'
+    },
+    {
+        manufacturer: 'Ford',
+        model: 'Kuga',
+        type: 'Diesel'
+    },
+    {
+        manufacturer: 'Seat',
+        model: 'Ibiza',
+        type: 'metano'
+    },
+    {
+        manufacturer: 'Audi',
+        model: 'R8',
+        type: 'Benzina'
+    },
+];
+
+const gasolineCars = cars.filter( (auto) => auto.type.toLowerCase() === 'benzina');
+
+const dieselCars = cars.filter( (auto) => auto.type.toLowerCase() === 'diesel');
+
+
+const otherCars = cars.filter( (auto) => {
+    return auto.type.toLowerCase() !== 'benzina' && auto.type.toLowerCase() !== 'diesel';
+});
+
+console.log('Auto a benzina');
+console.log('*******************************');
+console.log(gasolineCars);
+
+console.log('Auto a diesel');
+console.log('*******************************');
+console.log(dieselCars);
+
+console.log('Tutte le altre auto');
+console.log(otherCars);
+
+//1- Dato un array do oggetti, il programma stampa in console le auto in base al loro carburante.
+//2- manca una virgola a riga 60, riga 67 =>; a riga 69 serve un return, che possiamo mettere implicito scrivendo la funzione su una riga sola.
+//3- il carburante è spesso inserito con lettere maiuscole o meno, quindi senza cambiare il database è meglio dare un toLowerCase a tutte i valori. Riga 73, l'operatore è && e non ||.
